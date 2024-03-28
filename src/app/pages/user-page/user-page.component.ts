b5d7d343491ee022ce71c4dc5555cefaac913914
Mostafa Ellethy <mostafa.ellethy93@gmail.com>
2024-03-28 23:50:48 +0200

    feat: user page

--- src/app/pages/user-page/user-page.component.ts
+++ src/app/pages/user-page/user-page.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { UsersService } from '@/services';
+import { IUser } from '@/types';
+import { CommonModule, Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatProgressSpinner,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './user-page.component.html',
-  styleUrl: './user-page.component.scss'
+  styleUrl: './user-page.component.scss',
 })
-export class UserPageComponent {
+export class UserPageComponent implements OnInit {
+  //* Props
+  fetching = true;
+  user?: IUser;
 
+  //* Constructor
+  constructor(
+    public readonly location: Location,
+    private readonly usersService: UsersService,
+    private readonly route: ActivatedRoute
+  ) {}
+
+  //* Hooks
+  ngOnInit(): void {
+    this.usersService.find(this.route.snapshot.paramMap.get('id')!).subscribe({
+      next: ({ data }) => {
+        this.user = data;
+        this.fetching = false;
+      },
+    });
+  }
 }
